536ba6f306b57686c083fbe6c097d3c23205dd83
github-aliceyang <aliyang47@gmail.com>
2022-09-15 16:47:23 -0700

    added the form control

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,16 @@ import "./Weather.css";
 export default function Weather() {
     return (
         <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input type="search" placeholder="enter a city..." className="form-control" />
+                    </div>
+                    <div className="col-3">
+                        <input type="submit" value="Search" className="btn btn-primary" />
+                    </div>
+                </div>
+            </form>
             <div className="row">
                 <h1> San Jose</h1>
                 <ul>
