952227d2a65979927467c28f8afa83d44e9d463f
github-aliceyang <aliyang47@gmail.com>
2022-09-16 12:16:42 -0700

    used conditional rendering

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <Weather />
       </div>
-      <footer> This app was coded by Alice Yang and is <a href="/" >open-sourced on GitHub</a></footer>
+      <footer> This app was coded by Alice Yang and is <a href="https://github.com/Ayang471/react-project/tree/main" >open-sourced on GitHub</a></footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
 import axios from "axios";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 
 
 export default function Weather(props) {
-    const [weatherData, setWeatherData] = useState({});
+    const [weatherData, setWeatherData] = useState({ ready: false });
     function handleResponse(response) {
-        console.log(response.data)
+        console.log(response.data);
         setWeatherData({
+            ready: true,
             temperature: response.data.main.temp,
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
@@ -16,47 +17,52 @@ export default function Weather(props) {
             humidity: response.data.main.humidity,
             wind: response.data.wind.speed,
             icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-        })
+        });
 
     }
 
-    const apiKey = "bc2cd97eaa209e7d22d8f3c84081655f";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Jose&appid=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(handleResponse);
-
-
-    return (
-        <div className="Weather">
-            <form>
+    if (weatherData.ready) {
+        return (
+            <div className="Weather">
+                <form>
+                    <div className="row">
+                        <div className="col-9">
+                            <input type="search" placeholder="enter a city..." className="form-control" />
+                        </div>
+                        <div className="col-3">
+                            <input type="submit" value="Search" className="btn btn-primary" />
+                        </div>
+                    </div>
+                </form>
                 <div className="row">
-                    <div className="col-9">
-                        <input type="search" placeholder="enter a city..." className="form-control" />
-                    </div>
-                    <div className="col-3">
-                        <input type="submit" value="Search" className="btn btn-primary" />
-                    </div>
-                </div>
-            </form>
-            <div className="row">
-                <h1>{weatherData.city}</h1>
-                <ul className="daily-info">
-                    <li> <FormattedDate date={weatherData.date} /></li>
-                    <li className="text-capitalize"> {weatherData.description}</li>
-                </ul>
-            </div>
-            <div className="row">
-                <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Cloudy" />
-                    <span className="temperature">{Math.round(weatherData.temperature)}</span>
-                    <span className="units">°C | °F </span>
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>humidity: {weatherData.humidity} %</li>
-                        <li>Wind: {weatherData.wind} km/h</li>
+                    <h1>{weatherData.city}</h1>
+                    <ul className="daily-info">
+                        <li> <FormattedDate date={weatherData.date} /></li>
+                        <li className="text-capitalize"> {weatherData.description}</li>
                     </ul>
                 </div>
+                <div className="row">
+                    <div className="col-6">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Cloudy" />
+                        <span className="temperature">{Math.round(weatherData.temperature)}</span>
+                        <span className="units">°C | °F </span>
+                    </div>
+                    <div className="col-6">
+                        <ul>
+                            <li>humidity: {weatherData.humidity} %</li>
+                            <li>Wind: {weatherData.wind} km/h</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        const apiKey = "d1a86552de255334f6117b348c4519bd";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Jose&appid=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+
+
+        return "loading...";
+    }
+
 }
