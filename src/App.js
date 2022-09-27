ed6483ada23fc19201af891be3c34c3aeb8c3ec9
Maritssa1919 <103866109+maritssam@users.noreply.github.com>
2022-09-26 20:22:34 -0500

    update name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="San Jose" />
       </div>
-      <footer> This app was coded by Alice Yang and is <a href="https://github.com/Ayang471/react-project/tree/main" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      <footer> This app was coded by maarti medina and is <a href="https://github.com/Ayang471/react-project/tree/main" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
         and hosted on <a href="https://glowing-bonbon-bc9d7c.netlify.app/" target="_blank" rel="noreferrer"> Netlify.</a></footer>
     </div>
   );
